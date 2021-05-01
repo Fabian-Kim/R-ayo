fc0121564c0a994ba477a405f18abc38565e9902
Fabian-Kim <kor2wor@gmail.com>
2021-05-01 09:01:38 +0900

    ttest practice 2

--- class_fc/practice_part4.R
+++ class_fc/practice_part4.R
@@ -30,3 +30,26 @@ var.test(groupA[,2], groupB[,2])
 t.test(groupA[,2], groupB[,2],alternative = "less", var.equal = T)
 ## alternative less : 왼쪽 
 
+
+####
+#laod file
+rawN10 <- read.csv(file = "C:/Users/Fabian/Documents/RStudio/R-ayo/class_fc/sourceData/htest02.csv", header = T)
+rawN10
+
+#grouping
+groupA2 <- rawN10[rawN10$group=='A',1:2]
+groupB2 <- rawN10[rawN10$group=='B', 1:2]
+
+#mean
+mean(groupA2[,2])
+mean(groupB2[,2])
+
+#Normality
+shapiro.test(groupA2[,2]) #p-value = 0.2826 > 0.05
+shapiro.test(groupB2[,2]) #p-value = 0.9108 > 0.05
+
+#Variance
+var.test(groupA2[,2],groupB2[,2])
+
+#ttest
+t.test(groupA2[,2],groupB2[,2], alternative = "less", var.equal = F)
